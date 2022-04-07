45ac5add98cce9a6c06aa8feb2e8e74c1b62128a
AaronXue0 <her8153101@yahoo.com.tw>
2022-04-07 15:13:02 +0800

    Update phone puzzle

--- Assets/Scripts/Gameplays/PhonePassword.cs
+++ Assets/Scripts/Gameplays/PhonePassword.cs
@@ -30,6 +30,9 @@ namespace Innocence
 
         private void Update()
         {
+            if (isInAnimation)
+                return;
+
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
             if (hit && Input.GetMouseButtonDown(0) && isPickedUp)
             {
