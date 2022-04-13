07349d07a7b1eff5de7898d2a7aee1530e937e98
AaronXue0 <her8153101@yahoo.com.tw>
2022-04-13 12:58:03 +0800

    Add custom cursor

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseCursor : MonoBehaviour
+{
+    [SerializeField]
+    Sprite cursorSprite;
+
+    [SerializeField]
+    Vector2 offset;
+
+    private Image image;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        image = GetComponent<Image>();
+        image.sprite = cursorSprite;
+    }
+    private void Update()
+    {
+        Vector2 cursorPos = Input.mousePosition;
+        transform.position = cursorPos + offset;
+    }
+}
